80ab74a4fd2b1feaa912928299b3a5908aa3514a
Junghoon Kang <junghoon.jung.kang@gmail.com>
2015-11-27 17:23:06 -0500

    Update

--- src/main.py
+++ src/main.py
@@ -1,62 +1,79 @@
 from os import listdir
 from os.path import isfile, isdir, join
+import json
 
 from eventlog import *
 from btracelog import *
 
 class Info:
     def __init__(self):
-        self.application_id = None
-        self.configuration_id = None
-        self.application_name = None
-        self.input_size = None
+        self.app_id = None
+        self.conf_id = None
+        self.app_name = None
+        self.parameter = None
 
         self.running_time = None
-        self.gc_overhead = None
+        self.gc_time = None
         self.avg_cpu_usage = None
-        self.max_peak_memory = None
+        self.max_memory = None
 
     def __repr__(self):
         result = ""
-        result += "application_id = " + str(self.application_id) + "\n"
-        result += "configuation_id = " + str(self.configuration_id) + "\n"
-        result += "application_name = " + str(self.application_name) + "\n"
-        result += "input_size = " + str(self.input_size) + "\n"
+        result += "app_id = " + str(self.app_id) + "\n"
+        result += "conf_id = " + str(self.conf_id) + "\n"
+        result += "app_name = " + str(self.app_name) + "\n"
+        result += "parameter = " + str(self.parameter) + "\n"
         result += "running_time = " + str(self.running_time) + " (ms)\n"
-        result += "gc_overhead = " + str(self.gc_overhead) + " (ms)\n"
+        result += "gc_time = " + str(self.gc_time) + " (ms)\n"
         result += "avg_cpu_usage = " + str(self.avg_cpu_usage) + "\n"
-        result += "max_peak_memory = " + str(self.max_peak_memory) + " (MB)\n"
+        result += "max_memory = " + str(self.max_memory) + " (MB)\n"
         return result
+
+    def write_json(self, fname):
+        data = {}
+        data["app_id"] = self.app_id
+        data["conf_id"] = self.conf_id
+        data["app_name"] = self.app_name
+        data["parameter"] = self.parameter
+        data["running_time"] = self.running_time
+        data["gc_time"] = self.gc_time
+        data["avg_cpu_usage"] = self.avg_cpu_usage
+        data["max_memory"] = self.max_memory
+        f = open(fname, "w")
+        f.write(json.dumps(data, indent=4, separators=(',',': ')))
+        f.close()
+
 
 def main(string):
     result = Info()
 
+    if string[-1] == "/":
+        string = string[:-1]
     lst = string.split("/")
 
-    path = ""
-    directory = ""
-    flag = False
-    for i in range(len(lst)-1, -1, -1):
-        if flag == False and lst[i] != "":
-            directory = lst[i]
-            flag = True
-        elif flag == True:
-            path = lst[i] + "/" + path
-    if directory == "":
-        print "Invalid directory format."
-        return
+    path = "/".join(lst[:-1])  # path to input directory
+    if len(lst) == 2:
+        path += "/"
+    directory = lst[-1]  # input directory
 
     lst = directory.split("-")
     if len(lst) < 7:
         print "Invalid directory format."
         return
 
-    result.application_id = "-".join(lst[0:3])
-    result.configuration_id = "-".join(lst[3:6])
-    result.application_name = lst[6]
-
+    # parse directory name to get app id, name, and its parameters.
+    result.app_id = "-".join(lst[0:3])
+    result.conf_id = "-".join(lst[3:6])
+    result.app_name = lst[6]
+    result.parameter = "-".join(lst[7:])
 
     pd = path + directory
+
+    for f in listdir(pd):
+        if f == directory + ".js":
+            #print "Result already exists."
+            return
+
     eventlog_fname = ""
     for f in listdir(pd):
         if f.split("-")[0] == "app":
@@ -65,7 +82,7 @@ def main(string):
     # EventLog
     eventlog = EventLog(eventlog_fname)
     result.running_time = eventlog.app_runtime
-    result.gc_overhead = eventlog.gc_time
+    result.gc_time = eventlog.gc_time
 
     # BtraceLogs from all executors
     execdirs = sorted([ d for d in listdir(pd) if isdir(join(pd,d)) ])
@@ -75,16 +92,20 @@ def main(string):
             fname = join(pd,d,f)
             if isfile(fname) and fname.split(".")[-1] == "btrace":
                 btracelog_fnames.append(fname)
-
     btracelogs = []
     for fname in btracelog_fnames:
         btracelogs.append(BtraceLog(fname))
 
-    result.max_peak_memory = max([btracelog.max_memory for btracelog in btracelogs])
+    # Max peak memory among all executors
+    result.max_memory = max([btracelog.max_memory for btracelog in btracelogs])
+
+    # Avg CPU usage among all executors
     cpu = [btracelog.avg_cpu_load for btracelog in btracelogs]
     result.avg_cpu_usage = sum(cpu) / len(cpu)
 
-    print result
+    # Create a json file containing results
+    result.write_json(pd + "/" + directory + ".js")
+    #print result
 
 
 import sys
